1da3b5201950ade6d170567247b445bb7cd30b84
ruinername44 <abbasovrustam44@gmail.com>
2025-03-24 21:44:00 +0300

    add gitignore

--- src/main/java/homeworks/homework10/Main.java
+++ src/main/java/homeworks/homework10/Main.java
@@ -4,18 +4,15 @@ public class Main {
     public static void main(String[] args) {
         int[] array = {12, 15, 18, 21, 24};
 
-        // Проверка на четность элемента
         System.out.println("Четные элементы:");
         int[] evenNumbers = Sequence.filter(array, x -> x % 2 == 0);
         printArray(evenNumbers);
 
-        // Проверка, является ли сумма цифр элемента четной
         System.out.println("\nЭлементы с четной суммой цифр:");
         int[] evenSumElements = Sequence.filter(array, x -> Sequence.sumOfDigits(x) % 2 == 0);
         printArray(evenSumElements);
     }
 
-    // Функция для вывода массива
     private static void printArray(int[] arr) {
         for (int i : arr) {
             System.out.print(i + " ");
